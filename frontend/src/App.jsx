fedb5958763ecb260d27a334558965081b4cd5d9
fbisquerra <fbisquerra@travelcompositor.com>
2026-05-23 17:56:56 +0200

    Back to top: Add smooth scrolling and back to top component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar.jsx'
 import Home from './components/Home.jsx'
 import Experience from './components/Experience.jsx'
+import BackToTop from './components/BackToTop.jsx'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <Experience />
+      <BackToTop />
     </>
   )
 }
