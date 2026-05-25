57fffe6fb343abfdb31eb486f80f12044221c651
fbisquerra <fbisquerra@travelcompositor.com>
2026-05-25 21:13:42 +0200

    Profile: Fix unit test fixture

--- src/test/java/com/fbisquerra/aboutme/profile/fixtures/ProfileFixture.java
+++ src/test/java/com/fbisquerra/aboutme/profile/fixtures/ProfileFixture.java
@@ -1,6 +1,5 @@
 package com.fbisquerra.aboutme.profile.fixtures;
 
-import com.fbisquerra.aboutme.profile.application.dto.ProfileResponse;
 import com.fbisquerra.aboutme.profile.domain.model.Profile;
 
 import java.util.List;
@@ -14,26 +13,12 @@ public class ProfileFixture {
                 "Palma de Mallorca",
                 "francesc.bisquerra@gmail.com",
                 "https://www.linkedin.com/in/franbisquerra",
+                "https://github.com/FranBisquerra",
                 "I consider myself a passionate person.",
                 List.of(new Profile.Language("Catalan", "Native"), new Profile.Language("English", "Full Professional")),
                 List.of("Java", "Spring Boot", "Docker"),
                 List.of(new Profile.ExperienceEntry("Travel Compositor", "Full-stack Developer", "2022-03", null, "Development of a travel application.")),
                 List.of(new Profile.EducationEntry("Universitat de les Illes Balears", "Bachelor's Degree in Computer Engineering", "2012", "2019"))
-        );
-    }
-
-    public static ProfileResponse validProfileResponse() {
-        return new ProfileResponse(
-                "Francesc Bisquerra Castell",
-                "Software Engineer",
-                "Palma de Mallorca",
-                "francesc.bisquerra@gmail.com",
-                "https://www.linkedin.com/in/franbisquerra",
-                "I consider myself a passionate person.",
-                List.of(new ProfileResponse.Language("Catalan", "Native"), new ProfileResponse.Language("English", "Full Professional")),
-                List.of("Java", "Spring Boot", "Docker"),
-                List.of(new ProfileResponse.ExperienceEntry("Travel Compositor", "Full-stack Developer", "2022-03", null, "Development of a travel application.")),
-                List.of(new ProfileResponse.EducationEntry("Universitat de les Illes Balears", "Bachelor's Degree in Computer Engineering", "2012", "2019"))
         );
     }
 }
