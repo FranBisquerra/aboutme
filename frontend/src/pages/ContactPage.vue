5fe5877078ab0c961bd0ed15cc0fc37b588fbb8f
fbisquerra <fbisquerra@travelcompositor.com>
2026-05-26 22:32:26 +0200

    Contact me: adjust backgroud color #autodeploy

--- frontend/src/pages/ContactPage.vue
+++ frontend/src/pages/ContactPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <p class="text-2xl text-gray-500 dark:text-gray-400">Coming soon...</p>
+  <div class="flex items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-950">
+    <p class="text-2xl text-gray-400 dark:text-gray-500">Coming soon...</p>
   </div>
 </template>
