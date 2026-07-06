5b13463fb4fb80d6ea236dca1054990642b75f28
fbisquerra <fbisquerra@travelcompositor.com>
2026-07-06 19:13:54 +0200

    Undo code formatting for flyway scripts that invalidate the checksum #autodeploy

--- src/main/resources/db/migration/V3__create_users_table.sql
+++ src/main/resources/db/migration/V3__create_users_table.sql
@@ -1,5 +1,4 @@
-CREATE TABLE app_user
-(
+CREATE TABLE app_user (
     id            BIGINT       NOT NULL AUTO_INCREMENT,
     username      VARCHAR(100) NOT NULL,
     email         VARCHAR(255) NOT NULL,
